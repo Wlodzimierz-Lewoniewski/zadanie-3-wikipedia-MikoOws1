77a4af19f0a7100c3af46ff83582532ae7da2bcb
Mikolaj Owsianny <83515@student.ue.poznan.pl>
2024-10-29 13:36:42 +0100

    fix, requirements adjust

--- main.py
+++ main.py
@@ -1,105 +1,78 @@
+import re
 import requests
-from bs4 import BeautifulSoup
+import itertools
 
-def pobierz_pierwsze_dwa_artykuly(nazwa_kategorii):
-    url_kategorii = f'https://pl.wikipedia.org/wiki/Kategoria:{nazwa_kategorii.replace(" ", "_")}'
-    odpowiedz = requests.get(url_kategorii)
-    soup = BeautifulSoup(odpowiedz.content, 'html.parser')
-    mw_strony = soup.find('div', {'id': 'mw-pages'})
-    artykuly = []
-    if not mw_strony:
-        return artykuly
-    for ul in mw_strony.find_all('ul'):
-        for li in ul.find_all('li'):
-            a_tag = li.find('a', href=True)
-            if a_tag:
-                tytul_artykulu = a_tag.get('title').strip()
-                url_artykulu = 'https://pl.wikipedia.org' + a_tag.get('href').strip()
-                artykuly.append({'tytul': tytul_artykulu, 'url': url_artykulu})
-                if len(artykuly) >= 2:
-                    return artykuly
-    return artykuly
+wzor_wpisy = r'<li[^>]*>.*<a[^>]*href=\"(/wiki/(?![^"]*:)[^"]+)\"[^>]*title=\"([^"]+)\"[^>]*>.*</li>'
+szlink_wpis = r'<a[^>]*href=\"(/wiki/(?![^"]*:)[^"]+)\"[^>]*title=\"([^"]+)\"[^>]*>'
+pics_wpisy = r'<img[^>]*src=\"(//upload\.wikimedia\.org/[^"]+)\"[^>]*/>'
+linki_zew = r'<a[^>]*class=\"external[^"]*\"[^>]*href=\"([^"]+)\"[^>]*>'
+kat_strony = r'<a[^>]*href=\"(/wiki/Kategoria:[^"]+)\"[^>]*title=\"([^"]+)\"[^>]*>'
 
-def wyciagnij_linki_wewnetrzne(soup, limit=5):
-    linki = []
-    div_tresci = soup.find('div', {'id': 'mw-content-text'})
-    if not div_tresci:
-        return linki
-    for a_tag in div_tresci.find_all('a', href=True):
-        href = a_tag['href'].strip()
-        if href.startswith('/wiki/') and ':' not in href[6:]:
-            tytul_linku = a_tag.get('title').strip()
-            if tytul_linku:
-                linki.append(tytul_linku)
-                if len(linki) >= limit:
-                    break
-    return linki
+def zlacz_linie(lista):
+    polaczone = ' | '.join(lista)
+    print(polaczone)
 
-def wyciagnij_url_obrazkow(soup, limit=3):
-    obrazki = []
-    div_tresci = soup.find('div', {'id': 'mw-content-text'})
-    if not div_tresci:
-        return obrazki
-    for img_tag in div_tresci.find_all('img'):
-        img_src = img_tag.get('src').strip()
-        if img_src:
-            if img_src.startswith('//'):
-                img_src = img_src
-            elif img_src.startswith('/'):
-                img_src = 'https://pl.wikipedia.org' + img_src
-            obrazki.append(img_src)
-            if len(obrazki) >= limit:
-                break
-    return obrazki
+def fragment_html(html_str: str) -> str:
+    return html_str[html_str.find('<div id="mw-content-text"'):html_str.find('<div id="catlinks"')]
 
-def wyciagnij_zrodla_zewnetrzne(soup, limit=3):
-    zrodla = []
-    sekcja_przypisow = soup.find('ol', class_='references')
-    if not sekcja_przypisow:
-        return zrodla
-    for li in sekcja_przypisow.find_all('li'):
-        for a_tag in li.find_all('a', href=True):
-            href = a_tag['href'].replace("&amp;", "&").strip()
-            if href.startswith('http'):
-                zrodla.append(href)
-                if len(zrodla) >= limit:
-                    return zrodla
-    return zrodla
+def przypisy_html(html_str: str) -> str:
+    html_str = html_str[html_str.find('id="Przypisy"'):]
+    html_str = html_str[:html_str.find('<div class="mw-heading')]
+    return html_str
 
-def wyciagnij_kategorie(soup, limit=3):
-    kategorie = []
-    div_kategorii = soup.find('div', {'id': 'catlinks'})
-    if not div_kategorii:
-        return kategorie
-    for a_tag in div_kategorii.find_all('a'):
-        nazwa_kategorii = a_tag.get_text().strip()
-        if nazwa_kategorii != 'Kategorie':
-            kategorie.append(nazwa_kategorii)
-            if len(kategorie) >= limit:
-                break
-    return kategorie
+def html_kategorie(html_str: str) -> str:
+    return html_str[html_str.find('<div id="catlinks"'):]
 
-def przetworz_artykuly(artykuly):
-    for artykul in artykuly:
-        odpowiedz = requests.get(artykul['url'])
-        soup = BeautifulSoup(odpowiedz.content, 'html.parser')
-        linki_wewnetrzne = wyciagnij_linki_wewnetrzne(soup)
-        url_obrazkow = wyciagnij_url_obrazkow(soup)
-        zrodla_zewnetrzne = wyciagnij_zrodla_zewnetrzne(soup)
-        kategorie = wyciagnij_kategorie(soup)
+def szukaj_wzorce(wzorzec: str, tekst: str, flagi: int = 0, limit: int = 5) -> list:
+    return [dopasowanie.groups() for dopasowanie in itertools.islice(re.finditer(wzorzec, tekst, flags=flagi), limit)]
+
+def generuj_url(nazwa_kat: str) -> str:
+    sformatowana_kat = nazwa_kat.replace(' ', '_')
+    return f'https://pl.wikipedia.org/wiki/Kategoria:{sformatowana_kat}'
+
+def uzyskaj_artykuly(nazwa_kat: str, limit: int = 3) -> list[tuple[str, str]]:
+    adres_kategorii = generuj_url(nazwa_kat)
+    odp = requests.get(adres_kategorii)
+    html_str = odp.text
+    return szukaj_wzorce(wzor_wpisy, html_str, limit=limit)
+
+def uzyskaj_html_artykul(artykul_url: str) -> str:
+    odp = requests.get("https://pl.wikipedia.org" + artykul_url)
+    return odp.text
+
+def znajdz_linki_w_artykule(html_tresc: str, limit: int = 5) -> list[tuple[str, str]]:
+    tresc_glowna = fragment_html(html_tresc)
+    return szukaj_wzorce(szlink_wpis, tresc_glowna, limit=limit)
+
+def znajdz_obrazki(html_tresc: str, limit: int = 3) -> list:
+    tresc_glowna = fragment_html(html_tresc)
+    return szukaj_wzorce(pics_wpisy, tresc_glowna, limit=limit)
+
+def znajdz_linki_zewnetrzne(html_tresc: str, limit: int = 3) -> list:
+    sekcja_przypisy = przypisy_html(html_tresc)
+    return szukaj_wzorce(linki_zew, sekcja_przypisy, limit=limit)
+
+def znajdz_kategorie(html_tresc: str, limit: int = 3) -> list:
+    sekcja_kat = html_kategorie(html_tresc)
+    return szukaj_wzorce(kat_strony, sekcja_kat, limit=limit)
+
+def glowna():
+    kat_in = input().strip()
+    artykuly = uzyskaj_artykuly(kat_in)
+    for art_link, art_tytul in artykuly:
+        tresc_artykul = uzyskaj_html_artykul(art_link)
         
-        # Usuwanie białych znaków na końcach
-        linki_wewnetrzne = ' | '.join(linki_wewnetrzne).strip()
-        url_obrazkow = ' | '.join(url_obrazkow).strip()
-        zrodla_zewnetrzne = ' | '.join(zrodla_zewnetrzne).strip()
-        kategorie = ' | '.join(kategorie).strip()
+        wew_linki = znajdz_linki_w_artykule(tresc_artykul)
+        zlacz_linie([tytul for _, tytul in wew_linki])
         
-        print(linki_wewnetrzne)
-        print(url_obrazkow)
-        print(zrodla_zewnetrzne)
-        print(kategorie)
-
-if __name__ == "__main__":
-    nazwa_kategorii = input().strip()
-    artykuly = pobierz_pierwsze_dwa_artykuly(nazwa_kategorii)
-    przetworz_artykuly(artykuly)
+        obrazy = znajdz_obrazki(tresc_artykul)
+        zlacz_linie([url for url, in obrazy])
+        
+        zew_linki = znajdz_linki_zewnetrzne(tresc_artykul)
+        zlacz_linie([url for url, in zew_linki])
+        
+        kategorie = znajdz_kategorie(tresc_artykul)
+        zlacz_linie([nazwa.replace('Kategoria:', '') for _, nazwa in kategorie])
+    
+if __name__ == '__main__':
+    glowna()
